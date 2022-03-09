5b63e0371baca87274744872b696113a39ecdbae
Shitov Sergey <serodjashitov@yandex.ru>
2022-03-09 21:42:24 +0300

    Added configuration Spring Security and login form

--- src/main/java/com/rottenbeetle/myblog/controllers/BlogController.java
+++ src/main/java/com/rottenbeetle/myblog/controllers/BlogController.java
@@ -15,7 +15,6 @@ import java.util.stream.Collectors;
 @Controller
 @RequestMapping("/blog")
 public class BlogController {
-
     @Autowired
     private PostRepository postRepository;
 
--- src/main/java/com/rottenbeetle/myblog/controllers/MainController.java
+++ src/main/java/com/rottenbeetle/myblog/controllers/MainController.java
@@ -1,16 +1,17 @@
 package com.rottenbeetle.myblog.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
 
-    @RequestMapping({"/",""})
+    @GetMapping("/")
     public String mainPage(){
         return "index";
     }
-    @RequestMapping("/about")
+    @GetMapping("/about")
     public String about(){
         return "about";
     }
